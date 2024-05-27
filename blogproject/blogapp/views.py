1602eb5f1cd9d227eec1184606208396dd661602
Yugo TANAKA <tanaka@neugate.co.jp>
2024-05-27 23:59:17 +0900

    [Add] till chapter 4

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,3 +1,10 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+def index_view(request):
+    return render(request, "index.html")
